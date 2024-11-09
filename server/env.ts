321b296271e3d7cccb115e685e678abea91cb36b
Artem Novak <artemnovak06@gmail.com>
2024-11-09 16:44:27 +0100

    Add environment variable parsing with validation

---
+++ server/env.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+    DB_USER: z.string().min(1).default("postgres"),
+    DB_PASSWORD: z.string().min(1),
+    DB_HOST: z.string().min(1).default("localhost"),
+    DB_PORT: z.coerce.number().min(1000).max(65535).default(5432),
+    DB_NAME: z.string().min(1).default("postgres"),
+    ENV: z
+        .union([
+            z.literal("development"),
+            z.literal("testing"),
+            z.literal("production"),
+        ])
+        .default("development"),
+})
+
+const env = envSchema.parse(process.env);
+
+export default env;
