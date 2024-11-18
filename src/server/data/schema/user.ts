0211c0f42a74eb6722d62485e1fa0d1642277a9a
Artem Novak <artemnovak06@gmail.com>
2024-11-18 14:49:36 +0100

    Refactor database schema to reuse column builders

--- src/server/data/schema/user.ts
+++ src/server/data/schema/user.ts
@@ -1,32 +1,33 @@
-import { bigint, bigserial, char, pgTable, varchar } from "drizzle-orm/pg-core";
+import { char, pgTable, varchar } from "drizzle-orm/pg-core";
+import { autoId, autoIdExternal } from "@/server/data/schema/common";
 
 /**
  * Table with common user data across all authentication methods.
  */
 export const user = pgTable("user", {
-    id: bigserial({ mode: "bigint" }).primaryKey(),
+    id: autoId(),
 });
 
 /**
  * Column builder that configures a reference to a user entry.
  */
-export const userReference = bigint({ mode: "bigint" }).references(() => user.id);
+export const userReference = () => autoIdExternal().references(() => user.id);
 
 /**
  * User credentials - only for users who use credentials-based authentication.
  */
 export const userCredentials = pgTable("user_credentials", {
-    userId: userReference.primaryKey(),
+    userId: userReference().primaryKey(),
     email: varchar().unique().notNull(),
     passwordHash: char({ length: 60 }).notNull(), // bcrypt hash output is exactly 60 characters
 });
 
 export const userGoogle = pgTable("user_google", {
-    userId: userReference.primaryKey(),
+    userId: userReference().primaryKey(),
     sub: varchar().unique().notNull(), // Subject ID returned by Google, link between the external account and the MemoGarden account
 });
 
 export const userFacebook = pgTable("user_facebook", {
-    userId: userReference.primaryKey(),
+    userId: userReference().primaryKey(),
     sub: varchar().unique().notNull(), // Subject ID returned by Facebook, link between the external account and the MemoGarden account
 });
