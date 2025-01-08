86f5df0f7c8bbe13ebe65db79a623ce40d8da350
Artem Novak <artemnovak06@gmail.com>
2025-01-08 01:09:41 +0100

    Implement card update server action

--- src/server/actions/card.ts
+++ src/server/actions/card.ts
@@ -4,7 +4,7 @@ import { ModifyCardData, ModifyCardSchema } from "@/lib/validation-schemas";
 import { getUserIDInProtectedRoute } from "@/lib/server-utils";
 import { isDeckAccessible } from "@/server/data/services/deck";
 import { ResponseNotFound } from "@/lib/responses";
-import { createCard } from "@/server/data/services/card";
+import { createCard, editCard, isCardAccessible } from "@/server/data/services/card";
 
 /**
  * Creates a new deck using the given input data and assigns it to the currently logged-in user.
@@ -18,4 +18,17 @@ export async function createNewCard(data: ModifyCardData) {
     const userId = await getUserIDInProtectedRoute();
     if (!(await isDeckAccessible(userId, data.deckId))) return ResponseNotFound; // This can very rarely happen even with compliant use when the user deletes a deck on one device and then selects that deck in the card creation form on another device (which has stale data) and submits that request
     return createCard(data);
+}
+
+/**
+ * Updates an existing card using the given input data.
+ *
+ * @param data Card update input.
+ * @param id Card's ID.
+ */
+export async function updateCard(data: ModifyCardData, id: string) {
+    if (ModifyCardSchema.safeParse(data).error) return;
+    const userId = await getUserIDInProtectedRoute();
+    if (!(await isCardAccessible(userId, id))) return; // Under normal use, the client application will never request to update a card the currently logged-in user does not own. Therefore, if this branch is reached, the request was constructed maliciously and does not need proper error reporting
+    await editCard(id, data);
 }
